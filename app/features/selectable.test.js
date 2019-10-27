23113e2cc2a0bf58f3936a75d69e900ecdc19ab5
Dohyung Ahn <me@rinae.dev>
2019-10-27 23:15:45 +0900

    write first test of `selectable.js`

---
+++ app/features/selectable.test.js
@@ -0,0 +1,21 @@
+import test from 'ava'
+import { setupPptrTab, teardownPptrTab } from '../../tests/helpers'
+
+test.beforeEach(setupPptrTab)
+
+test('Show overlay UI when hover the element', async t => {
+  const {page} = t.context;
+  await page.hover('.dark.artboard.message')
+  t.not(await page.$('visbug-hover'), null)
+  t.pass()
+})
+
+test('Does not show overlay UI when hover the iframe element', async t => {
+  const {page} = t.context;
+  await page.hover('iframe')
+  t.is(await page.$('visbug-hover'), null)
+  t.pass()
+})
+
+
+test.afterEach(teardownPptrTab)
